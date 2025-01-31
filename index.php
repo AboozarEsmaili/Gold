ed86167ffa3b44943ba71abf86929d85994cde32
AboozarEsmaili <aboozaresmailinet@gmail.com>
2025-01-31 21:06:44 +0330

    coffee

---
+++ index.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body class="w-sm h-screen bg-gray-400">
+    
+</body>
+</html>
